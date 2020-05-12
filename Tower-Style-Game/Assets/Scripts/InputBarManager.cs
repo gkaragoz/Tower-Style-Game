acaa56c620cd1dba573fa2751cee0a895b79951e
gkaragoz <gkaragoz96@gmail.com>
2020-05-12 15:06:12 +0300

    Hide and show InputBar.

--- Tower-Style-Game/Assets/Scripts/InputBarManager.cs
+++ Tower-Style-Game/Assets/Scripts/InputBarManager.cs
@@ -1,13 +1,18 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace GK {
 
 	public class InputBarManager : MonoBehaviour {
 
 		[SerializeField]
+		private InputManager _inputManager = null;
+		[SerializeField]
 		private InputBarSlot[] _slots = null;
 		[SerializeField]
 		private RectTransform _parentTransform = null;
+		[SerializeField]
+		private CanvasGroup _parentCanvasGroup = null;
 		[SerializeField]
 		private RectTransform _cursorTransform = null;
 		[SerializeField]
@@ -22,9 +27,27 @@ namespace GK {
 		private Vector2 _cursorStartPosition;
 		[SerializeField]
 		private Vector2 _cursorEndPosition;
+		[SerializeField]
+		private int _selectedIndex;
 
 		private void Awake() {
 			Init();
+			Hide();
+
+			_inputManager.OnInputBegin += OnInputBegin;
+			_inputManager.OnInputDragging += OnInputDragging;
+			_inputManager.OnInputEnd += OnInputEnd;
+		}
+
+		private void OnInputBegin(Vector2 startPosition) {
+			Show();
+		}
+
+		private void OnInputDragging(Vector2 draggingPosition, Vector2 direction) {
+		}
+
+		private void OnInputEnd(Vector2 endPosition, Vector2 direction) {
+			Hide();
 		}
 
 		private void Init() {
@@ -51,21 +74,26 @@ namespace GK {
 			CheckCursorIndex();
 		}
 
-		private int currentindex;
-
-		private void CheckCursorIndex() {
-			currentindex = (int)(_cursorTransform.anchoredPosition.x / _slotPerDistance);
+		private void Hide() {
+			_parentCanvasGroup.alpha = 0;
 		}
 
-		private void Play() {
+		private void Show() {
+			_parentCanvasGroup.alpha = 1;
+		}
+
+		private void CheckCursorIndex() {
+			_selectedIndex = (int)(_cursorTransform.anchoredPosition.x / _slotPerDistance);
+		}
+
+		public void Play() {
 			_cursorTransform.anchoredPosition = _cursorStartPosition;
 
 			LeanTween.moveLocalX(_cursorTransform.gameObject, _cursorEndPosition.x, _cursorSpeed).setLoopPingPong();
 		}
 
-		private void Stop() {
+		public void Stop() {
 			LeanTween.cancel(_cursorTransform.gameObject);
-			Debug.Log(currentindex);
 		}
 
 
