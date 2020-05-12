9d14a51e54fc9b318d137868b99b14576cda9c6e
gkaragoz <gkaragoz96@gmail.com>
2020-05-12 13:16:56 +0300

    Add core input mechanic feature of direction.

---
+++ Tower-Style-Game/Assets/Scripts/InputManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+
+namespace GK {
+
+	public class InputManager : MonoBehaviour {
+
+		public Action<Vector2> OnInputBegin;
+		public Action<Vector2, Vector2> OnInputDragging;
+		public Action<Vector2, Vector2> OnInputEnd;
+
+		private Vector2 _startPosition = Vector2.zero;
+		private Vector2 _endPosition = Vector2.zero;
+		private Vector2 _currentPosition = Vector2.zero;
+
+		private Vector2 _direction = Vector2.zero;
+
+		public Vector2 Direction {
+			get {
+				return _direction;
+			}
+		}
+
+		private void Update() {
+			if (Input.GetMouseButtonDown(0)) {
+				Vector2 mousePos = Input.mousePosition;
+				_startPosition = Camera.main.ScreenToWorldPoint(mousePos);
+				_currentPosition = _startPosition;
+
+				OnInputBegin?.Invoke(_startPosition);
+			}
+
+			if (Input.GetMouseButton(0)) {
+				Vector2 mousePos = Input.mousePosition;
+				_currentPosition = Camera.main.ScreenToWorldPoint(mousePos);
+				_direction = _startPosition - _currentPosition;
+
+				OnInputDragging?.Invoke(_currentPosition, _direction.normalized);
+			}
+
+			if (Input.GetMouseButtonUp(0)) {
+				Vector2 mousePos = Input.mousePosition;
+				_endPosition = Camera.main.ScreenToWorldPoint(mousePos);
+
+				_direction = _startPosition -_currentPosition;
+				OnInputEnd?.Invoke(_endPosition, _direction.normalized);
+
+				ResetInputs();
+			}
+		}
+
+		private void ResetInputs() {
+			_startPosition = Vector2.zero;
+			_endPosition = Vector2.zero;
+			_currentPosition = Vector2.zero;
+			_direction = Vector2.zero;
+		}
+
+		private void OnDrawGizmos() {
+			Gizmos.color = Color.white;
+			Gizmos.DrawLine(_startPosition, _currentPosition);
+		}
+	}
+
+}
---
+++ Tower-Style-Game/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace GK {
+
+	public class PlayerMotor : MonoBehaviour {
+
+		[SerializeField]
+		private float _jumpForce = 1f;
+		[SerializeField]
+		private ForceMode2D _forceMode2D = ForceMode2D.Impulse;
+
+		private Rigidbody2D _rb2D = null;
+		private InputManager _inputManager = null;
+
+		private void Awake() {
+			_rb2D = GetComponent<Rigidbody2D>();
+			_inputManager = GetComponent<InputManager>();
+
+			_inputManager.OnInputBegin += OnInputBegin;
+			_inputManager.OnInputDragging += OnInputDragging;
+			_inputManager.OnInputEnd += OnInputEnd;
+		}
+
+		private void OnInputBegin(Vector2 startPosition) {
+			//throw new NotImplementedException();
+		}
+
+		private void OnInputDragging(Vector2 draggingPosition, Vector2 direction) {
+			//throw new NotImplementedException();
+		}
+
+		private void OnInputEnd(Vector2 endPosition, Vector2 direction) {
+			_rb2D.AddForce(direction * _jumpForce, _forceMode2D);
+		}
+
+	}
+
+}
