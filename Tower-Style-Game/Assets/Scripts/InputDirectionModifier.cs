4787fed174ece5915d367898ac12403ab72377de
gkaragoz <gkaragoz96@gmail.com>
2020-05-13 13:20:21 +0300

    Refactor and format InputDirectionModifier.cs

--- Tower-Style-Game/Assets/Scripts/InputDirectionModifier.cs
+++ Tower-Style-Game/Assets/Scripts/InputDirectionModifier.cs
@@ -1,76 +1,65 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-namespace GK
-{
+namespace GK {
+
     /// <summary>
     /// /// Quaternion Vector Rotation Check
     /// </summary>/
-    public  class InputDirectionModifier:MonoBehaviour
-    {
+    public class InputDirectionModifier {
 
-        static private float _tanValue = .5f;
+        private static float _tanValue = .2f;
 
-        public static Vector2 UserDirectionVector(Vector2 direction)
-        {
+        private static bool IsInFirstArea(Vector2 direction) {
+            return direction.x > 0 && direction.y > 0;
+        }
+
+        private static bool IsInSecondArea(Vector2 direction) {
+            return direction.x < 0 && direction.y > 0;
+        }
+
+        private static bool IsInThirdArea(Vector2 direction) {
+            return direction.x < 0 && direction.y < 0;
+        }
+
+        private static bool IsInFourthArea(Vector2 direction) {
+            return direction.x > 0 && direction.y < 0;
+        }
+
+        public static Vector2 UserDirectionVector(Vector2 direction) {
             return -1 * CalculateVector(direction);
-
         }
 
-        public static Vector2 InputDirectionVector(Vector2 direction)
-        {
+        public static Vector2 InputDirectionVector(Vector2 direction) {
             return CalculateVector(direction);
-
         }
 
-
-
-        private static Vector2 CalculateVector(Vector2 direction)
-        {            
+        private static Vector2 CalculateVector(Vector2 direction) {
             Vector2 modifiedVector = Vector2.zero;
             float dividedValue = direction.y / direction.x;
-            
 
             ///if the touch is dragging to positive side
-            if (direction.x > 0 && direction.y < 0|| direction.x > 0 && direction.y > 0)
-            {
-                if (dividedValue > _tanValue)
-                {
+            if (IsInFourthArea(direction) || IsInFirstArea(direction)) {
+                if (dividedValue > _tanValue) {
                     modifiedVector = direction;
-                }
-                else
-                {
+                } else {
                     modifiedVector = new Vector2(direction.x, direction.y / dividedValue * _tanValue);
-
                 }
 
                 return modifiedVector;
             }
+            
             ///if the touch is dragging to negative side
-
-            if (direction.x < 0 && direction.y < 0|| direction.x < 0 && direction.y > 0)
-            {
-                if (dividedValue < -_tanValue)
-                {
+            if (IsInThirdArea(direction) || IsInSecondArea(direction)) {
+                if (dividedValue < -_tanValue) {
                     modifiedVector = direction;
-                }
-                else
-                {
+                } else {
                     modifiedVector = new Vector2(direction.x, direction.y / dividedValue * -_tanValue);
-
                 }
 
                 return modifiedVector;
             }
-
-
 
             return modifiedVector;
         }
-
-
-
-
     }
 }
