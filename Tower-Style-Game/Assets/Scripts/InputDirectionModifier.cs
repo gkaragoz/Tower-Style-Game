8a792312170f28a25e25280165e7fe3d419c7cd4
YesilyurtGurkan <gurkan@ganover.com>
2020-05-13 10:19:58 +0300

    Refactor InputDirectionModifier extension method

--- Tower-Style-Game/Assets/Scripts/InputDirectionModifier.cs
+++ Tower-Style-Game/Assets/Scripts/InputDirectionModifier.cs
@@ -7,99 +7,66 @@ namespace GK
     /// <summary>
     /// /// Quaternion Vector Rotation Check
     /// </summary>/
-    public class InputDirectionModifier : MonoBehaviour
+    public  class InputDirectionModifier:MonoBehaviour
     {
 
         static private float _tanValue = .5f;
 
         public static Vector2 UserDirectionVector(Vector2 direction)
         {
-            return CalculateVector(direction);
+            return -1 * CalculateVector(direction);
 
         }
 
         public static Vector2 InputDirectionVector(Vector2 direction)
         {
-            return -1*CalculateVector(direction);
+            return CalculateVector(direction);
 
         }
 
 
 
         private static Vector2 CalculateVector(Vector2 direction)
-        {
-            Vector2 trueVector = -direction;
-            Vector2 modifiedVector1 = Vector2.zero;
-            float dividedValue = trueVector.y / trueVector.x;
+        {            
+            Vector2 modifiedVector = Vector2.zero;
+            float dividedValue = direction.y / direction.x;
+            
 
-
-            if (trueVector.x > 0 && trueVector.y > 0)
-            {
-                if (dividedValue < -_tanValue)
-                {
-                    modifiedVector1 = trueVector;
-                }
-                else
-                {
-
-                    modifiedVector1 = new Vector2(trueVector.x, trueVector.y / dividedValue * -_tanValue);
-
-                }
-
-                return modifiedVector1;
-
-
-            }
-
-            if (trueVector.x > 0 && trueVector.y < 0)
-            {
-                if (dividedValue < -_tanValue)
-                {
-                    modifiedVector1 = trueVector;
-                }
-                else
-                {
-                    modifiedVector1 = new Vector2(trueVector.x, trueVector.y / dividedValue * -_tanValue);
-
-                }
-
-                return modifiedVector1;
-            }
-
-            if (trueVector.x < 0 && trueVector.y < 0)
+            ///if the touch is dragging to positive side
+            if (direction.x > 0 && direction.y < 0|| direction.x > 0 && direction.y > 0)
             {
                 if (dividedValue > _tanValue)
                 {
-                    modifiedVector1 = trueVector;
+                    modifiedVector = direction;
                 }
                 else
                 {
-                    modifiedVector1 = new Vector2(trueVector.x, trueVector.y / dividedValue * _tanValue);
+                    modifiedVector = new Vector2(direction.x, direction.y / dividedValue * _tanValue);
 
                 }
 
-                return modifiedVector1;
+                return modifiedVector;
             }
+            ///if the touch is dragging to negative side
 
-
-
-            if (trueVector.x < 0 && trueVector.y > 0)
+            if (direction.x < 0 && direction.y < 0|| direction.x < 0 && direction.y > 0)
             {
-                if (dividedValue > _tanValue)
+                if (dividedValue < -_tanValue)
                 {
-                    modifiedVector1 = trueVector;
+                    modifiedVector = direction;
                 }
                 else
                 {
-                    modifiedVector1 = new Vector2(trueVector.x, trueVector.y / dividedValue * _tanValue);
+                    modifiedVector = new Vector2(direction.x, direction.y / dividedValue * -_tanValue);
 
                 }
 
-                return modifiedVector1;
+                return modifiedVector;
             }
 
 
-            return modifiedVector1;
+
+            return modifiedVector;
         }
 
 
