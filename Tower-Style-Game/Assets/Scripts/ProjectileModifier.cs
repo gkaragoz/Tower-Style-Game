8f084919ee7a06d39b8a4080b52d5b5b340d5654
YesilyurtGurkan <gurkan@ganover.com>
2020-05-14 17:08:50 +0300

    Refactor Input Projectile Modifier

--- Tower-Style-Game/Assets/Scripts/ProjectileModifier.cs
+++ Tower-Style-Game/Assets/Scripts/ProjectileModifier.cs
@@ -13,16 +13,22 @@ public class ProjectileModifier : MonoBehaviour
         private GameObject _singleBall=null;
         [SerializeField]
         private int _projectileCount;
+        [SerializeField]
+        private float _localScaleDivider;
+        [SerializeField]
+        private float projectileLenghtMultiplier;
 
+        private Vector3 _startPos;
+        private Vector3 _baseLocalScale;
         private GameObject[] _balls;
 
-        Vector3 startPos;
 
         private void Start() {
             InputManager.instance.OnInputBegin += OnInputBegin;
             InputManager.instance.OnInputDragging += OnInputDragging;
             InputManager.instance.OnInputEnd += OnInputEnd;
 
+            _baseLocalScale = _singleBall.transform.localScale;
             _balls = new GameObject[_projectileCount];
 
             for (int i = 0; i < _projectileCount; i++) {
@@ -34,7 +40,7 @@ public class ProjectileModifier : MonoBehaviour
 
         private void OnInputBegin(Vector2 startPos) {
 
-            this.startPos = startPos;
+            _startPos = startPos;
         }
 
         private void OnInputDragging(Vector2 currentPos, Vector2 dragPos) {
@@ -44,16 +50,23 @@ public class ProjectileModifier : MonoBehaviour
         private void OnInputEnd(Vector2 startPos, Vector2 endPos, float arg3) {
             for (int i = 0; i < _balls.Length; i++) {
                 _balls[i].SetActive(false);
+                _balls[i].transform.localScale = _baseLocalScale;
             }
         }
 
 
         private void SetPositionProjectiles(Vector3 currentPos,Vector3 dragPos) {
-            Vector3 direction = currentPos - startPos;
-            Vector3 distanceProjectile = direction / _balls.Length;
+            /* Vector3 direction = currentPos - _startPos;
+             Vector3 distanceProjectile = direction / _balls.Length;*/ // for Infinity Projectiles
+
+            Vector3 distanceProjectile = dragPos / _balls.Length;
+
+
             for (int i = 0; i < _balls.Length; i++) {
                 _balls[i].SetActive(true);
-                _balls[i].transform.position = playerPos.position -  distanceProjectile * i;
+                _balls[i].transform.position = playerPos.position +  distanceProjectile * i*projectileLenghtMultiplier;
+                _balls[i].transform.localScale = _baseLocalScale/(_localScaleDivider*i+1); 
+
             }
         }
     }
