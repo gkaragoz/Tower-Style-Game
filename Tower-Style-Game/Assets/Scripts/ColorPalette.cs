e60c71412935f68602eecd689295157ab3873fb4
gkaragoz <gkaragoz96@gmail.com>
2020-05-14 14:24:29 +0300

    Create RandomPalette and RandomColor scripts.

---
+++ Tower-Style-Game/Assets/Scripts/ColorPalette.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace GK {
+
+	public class ColorPalette : MonoBehaviour {
+
+		#region Singleton
+
+		public static ColorPalette instance;
+		private void Awake() {
+			if (instance == null)
+				instance = this;
+			else if (instance != this)
+				Destroy(gameObject);
+		}
+
+		#endregion
+
+		[SerializeField]
+		private Color[] _colors;
+
+		public Color GetRandomColor() {
+			return _colors[Random.Range(0, _colors.Length)];
+		}
+
+	}
+
+}
---
+++ Tower-Style-Game/Assets/Scripts/RandomColor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace GK {
+
+	public class RandomColor : MonoBehaviour {
+
+		private void Start() {
+			// You can re-use this block between calls rather than constructing a new one each time.
+			var block = new MaterialPropertyBlock();
+
+			// You can look up the property by ID instead of the string to be more efficient.
+			block.SetColor("_BaseColor", ColorPalette.instance.GetRandomColor());
+
+			// You can cache a reference to the renderer to avoid searching for it.
+			GetComponent<Renderer>().SetPropertyBlock(block);
+		}
+
+	}
+
+}
