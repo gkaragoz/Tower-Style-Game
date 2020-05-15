d34af0b673b3274a00c759bc1ec9711bca8510fb
gkaragoz <gkaragoz96@gmail.com>
2020-05-15 12:35:18 +0300

    Import Utils classes.

---
+++ Tower-Style-Game/Assets/Scripts/Utils/ExtensionMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class ExtensionMethods {
+
+    public static float Map(this float value, float fromSource, float toSource, float fromTarget, float toTarget) {
+        return (value - fromSource) / (toSource - fromSource) * (toTarget - fromTarget) + fromTarget;
+    }
+
+    public static bool AmIAbleToBuyIt(int myCurrencyAmount, float price) {
+        if (myCurrencyAmount - price < 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    public static IEnumerable<TSource> DistinctBy<TSource, TKey>
+    (this IEnumerable<TSource> source, Func<TSource, TKey> keySelector) {
+        HashSet<TKey> seenKeys = new HashSet<TKey>();
+        foreach (TSource element in source) {
+            if (seenKeys.Add(keySelector(element))) {
+                yield return element;
+            }
+        }
+    }
+
+}
+
---
+++ Tower-Style-Game/Assets/Scripts/Utils/PlayerPrefsCleaner.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class PlayerPrefsCleaner : MonoBehaviour {
+
+    private void Awake() {
+        PlayerPrefs.DeleteAll();
+    }
+
+}
