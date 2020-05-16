c7ac0cba6c8f67034802f48fa1e7587f6d6f0a15
gkaragoz <gkaragoz96@gmail.com>
2020-05-16 13:59:57 +0300

    Create PlayerAnimation script. Implement basics.

---
+++ Tower-Style-Game/Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+namespace GK {
+
+	public class PlayerAnimation : MonoBehaviour {
+
+		private Animator _animator;
+		private PlayerGroundChecker _playerGroundChecker;
+		private PlayerMotor _playerMotor;
+
+		private const string JUMP_BEGIN = "JumpBegin";
+		private const string JUMP_START = "JumpStart";
+		private const string JUMP_PEEK = "JumpPeek";
+		private const string JUMP_END = "JumpEnd";
+		private const string JUMP_WALL = "JumpWall";
+		private const string INPUT = "Input";
+
+		private void Awake() {
+			_animator = GetComponentInChildren<Animator>();
+			_playerGroundChecker = GetComponent<PlayerGroundChecker>();
+			_playerMotor = GetComponent<PlayerMotor>();
+		}
+
+		private void Start() {
+			_playerGroundChecker.OnGrounded += OnGrounded;
+			_playerGroundChecker.OnSliding += OnSliding;
+			_playerMotor.OnJumped += OnJumped;
+			_playerMotor.OnPeek += OnPeek;
+
+			InputManager.instance.OnInputBegin += OnInputBegin;
+			InputManager.instance.OnInputDragging += OnInputDragging;
+		}
+
+		private void OnInputBegin(Vector2 startPosition) {
+			_animator.SetTrigger(JUMP_BEGIN);
+		}
+
+		private void OnInputDragging(Vector2 draggingPosition, Vector2 direction) {
+			
+		}
+
+		private void OnJumped() {
+
+		}
+		private void OnPeek() {
+
+		}
+
+		private void OnGrounded() {
+
+		}
+
+		private void OnSliding(PlayerGroundChecker.Direction direction) {
+			
+		}
+	}
+
+}
--- Tower-Style-Game/Assets/Scripts/Player/PlayerMotor.cs
+++ Tower-Style-Game/Assets/Scripts/Player/PlayerMotor.cs
@@ -5,19 +5,40 @@ namespace GK {
 
 	public class PlayerMotor : MonoBehaviour {
 
+		public Action OnJumped;
+		public Action OnPeek;
+
 		[SerializeField]
 		private float _baseJumpForce = 1f;
 		[SerializeField]
 		private ForceMode2D _forceMode2D = ForceMode2D.Impulse;
 
 		private Rigidbody2D _rb2D = null;
+		private PlayerGroundChecker _playerGroundChecker;
 
 		private void Start() {
 			_rb2D = GetComponent<Rigidbody2D>();
+			_playerGroundChecker = GetComponent<PlayerGroundChecker>();
+		}
+
+		private void FixedUpdate() {
+			if (IsPeeked()) {
+				OnPeek?.Invoke();
+			}
+		}
+
+		private bool IsPeeked() {
+			if (_rb2D.velocity.y == 0 && _playerGroundChecker.IsGrounded == false) {
+				return true;
+			} else {
+				return false;
+			}
 		}
 
 		public void Jump(Vector2 direction, float selectedInputPower) {
 			_rb2D.AddForce(direction * _baseJumpForce * selectedInputPower, _forceMode2D);
+
+			OnJumped?.Invoke();
 		}
 
 	}
