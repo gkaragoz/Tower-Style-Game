7c42588cd277eff09a2d9a771c8bf23f20487e63
YesilyurtGurkan <gurkan@ganover.com>
2020-05-20 13:33:40 +0300

    Add Laser button

---
+++ Tower-Style-Game/Assets/Scripts/Laser/LaserButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GY{
+
+public class LaserButton : MonoBehaviour
+ {
+        [SerializeField]
+        private LaserHolder _laserHolder;
+
+        public void CloseLaser() {
+            _laserHolder.CloseLaser();
+        }
+ }
+}
---
+++ Tower-Style-Game/Assets/Scripts/Laser/LaserHolder.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GK;
+
+namespace GY{
+
+public class LaserHolder : MonoBehaviour
+ {
+        [SerializeField]
+        private float _laserCloseSpeed=1;
+        private Coroutine _closeLaser;
+
+        public void CloseLaser() {
+
+            _closeLaser = StartCoroutine(CloseLaserStart());
+        }
+
+
+
+        IEnumerator CloseLaserStart() {
+            while (true) {
+                transform.localScale = new Vector3(Mathf.MoveTowards(transform.localScale.x,0,Time.deltaTime*_laserCloseSpeed),transform.localScale.y,transform.lossyScale.z);
+                if (transform.localScale.x<0.1f&&transform.localScale.x>-.1f) {
+                    StopCoroutine(_closeLaser);
+                    transform.gameObject.SetActive(false);
+                }
+                yield return null;
+            }
+        }
+ }
+}
