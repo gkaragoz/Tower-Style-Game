c17cef26c0a11f750ffd34de4c8948c8ed6000ff
YesilyurtGurkan <gurkan@ganover.com>
2020-05-20 18:15:33 +0300

    Add basic fire

---
+++ Tower-Style-Game/Assets/Scripts/Fire/FireHolder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GY{
+
+public class FireHolder : MonoBehaviour
+ {
+        [SerializeField]
+        private  float fireLenght=5;
+        [SerializeField]
+        private float fireSpeed=1;
+        [SerializeField]
+        private float startWaitTime = 1;
+        [SerializeField]
+        private float endWaitTime = 1;
+        private void Start() {
+            OnStart();
+            
+        }
+
+        public void OnComp() {
+            LeanTween.scaleX(transform.gameObject, 0, fireSpeed).setDelay(startWaitTime).setOnComplete(OnStart);
+        }
+        public void OnStart() {
+            LeanTween.scaleX(transform.gameObject, fireLenght, fireSpeed).setDelay(endWaitTime).setOnComplete(OnComp);
+
+        }
+    }
+}
