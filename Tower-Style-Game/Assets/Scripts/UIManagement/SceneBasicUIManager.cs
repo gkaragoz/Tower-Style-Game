3db042f03c40031eb02ccc8d471c5181a5be55a2
YesilyurtGurkan <gurkan@ganover.com>
2020-05-23 15:35:57 +0300

    Implement UI

---
+++ Tower-Style-Game/Assets/Scripts/UIManagement/SceneBasicUIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GY{
+
+public class SceneBasicUIManager : MonoBehaviour
+ {
+        private int _goldCount;
+
+        [SerializeField]
+        private Text _coinText;
+
+
+        public void UpdateGold(int goldCount) {
+            _coinText.text = "Gold : " + goldCount;
+        }
+ }
+}
