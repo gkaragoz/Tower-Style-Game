dd194701e3fe13aab68ba0b008b1103d62f60533
YesilyurtGurkan <gurkan@ganover.com>
2020-05-23 17:33:43 +0300

    Refactor scripts part2

--- Tower-Style-Game/Assets/Scripts/UIManagement/SceneBasicUIManager.cs
+++ Tower-Style-Game/Assets/Scripts/UIManagement/SceneBasicUIManager.cs
@@ -3,18 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace GY{
-
-public class SceneBasicUIManager : MonoBehaviour
- {
-        private int _goldCount;
-
+namespace GY {
+    public class SceneBasicUIManager : MonoBehaviour {
+        private int _goldCount=0;
         [SerializeField]
-        private Text _coinText;
-
-
+        private Text _coinText=null;
         public void UpdateGold(int goldCount) {
             _coinText.text = "Gold : " + goldCount;
         }
- }
+    }
 }
