1c8029b6eab2829b876a2dcb70ce9b646052e7c2
YesilyurtGurkan <gurkan@ganover.com>
2020-05-23 15:38:23 +0300

    Implement Collisison Dedector

---
+++ Tower-Style-Game/Assets/Scripts/Player/CollisionDedector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GY{
+
+public class CollisionDedector : MonoBehaviour
+ {
+        //Just for today
+        int _goldCount=0;
+        [SerializeField]
+        private SceneBasicUIManager sceneUIManager;
+
+        private void OnTriggerEnter2D(Collider2D collision) {
+            if (collision.gameObject.tag == "Gold") {
+                CollectGold(collision.gameObject);
+            }
+        }
+
+        public void CollectGold(GameObject gold) {
+            gold.SetActive(false);
+            _goldCount += 1;
+            sceneUIManager.UpdateGold(_goldCount);
+        }
+
+    }
+}
