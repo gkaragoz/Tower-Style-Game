f590924776b77f5f8e35a56a819c7336ec15c6a3
gkaragoz <gkaragoz96@gmail.com>
2020-05-30 20:13:40 +0300

    Create OnclickTween.cs script.

---
+++ Tower-Style-Game/Assets/Scripts/UIManagement/OnclickTween.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GK {
+
+	[RequireComponent(typeof(Button))]
+	public class OnclickTween : MonoBehaviour {
+
+		[SerializeField]
+		private RectTransform _targetTween = null;
+		[SerializeField]
+		private float _scaleAmount = 1.1f;
+		[SerializeField]
+		private float _scaleSpeed = 0.5f;
+
+		private Button _button;
+
+		private void Awake() {
+			_button = GetComponent<Button>();
+
+			_button.onClick.AddListener(() => {
+				if (LeanTween.isTweening(_targetTween)) {
+					return;
+				}
+
+				// Pop size of button
+				LeanTween.size(_targetTween, _targetTween.sizeDelta * _scaleAmount, _scaleSpeed).setEaseInOutCirc().setRepeat(2).setLoopPingPong();
+			});
+		}
+
+	}
+
+}
