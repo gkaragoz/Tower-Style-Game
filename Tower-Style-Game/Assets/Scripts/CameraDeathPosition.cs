1cfbbb30e278484f84964c84c90daaf7a4e5f687
YesilyurtGurkan <gurkan@ganover.com>
2020-05-30 13:48:21 +0300

    Finalize UI Manager

--- Tower-Style-Game/Assets/Scripts/CameraDeathPosition.cs
+++ Tower-Style-Game/Assets/Scripts/CameraDeathPosition.cs
@@ -21,9 +21,18 @@ public class CameraDeathPosition : MonoBehaviour
         private float _lavaYPosition=0;
         [SerializeField]
         private CameraController _cameraController=null;
+        [SerializeField]
+        private UIManager _uIManager;
+        [SerializeField]
+        [Utils.ReadOnly]
+        private float _endGameHeight;
 
         private void Start() {
             _lavaYPosition = -10f;
+            _playerMaxPoint = 0;
+            _endGameHeight = GameObject.FindGameObjectWithTag("EndGameArea").transform.position.y;
+
+
         }
 
         public float LavaYPosition() {
@@ -31,7 +40,9 @@ public class CameraDeathPosition : MonoBehaviour
         }
 
         private void Update() {
-            if(_player.position.y>_playerMaxPoint) {
+            _uIManager.UpdateIndicator(_playerMaxPoint/_endGameHeight);
+
+            if (_player.position.y>_playerMaxPoint) {
                 _playerMaxPoint = _player.position.y;
                _lavaYPosition= _playerMaxPoint - _deathZoneDistance;
                 _collector.transform.position = Vector3.up * _lavaYPosition;
@@ -44,6 +55,7 @@ public class CameraDeathPosition : MonoBehaviour
         public void GameOver() {
             _cameraController.target = null;
             Debug.Log("Oyun Bitti");
+            _uIManager.OpenFailPanel();
             _isGameOver = true;
         }
     }
