727e08d23a2f49299cd3da1fa00ccfdcbf18357a
gkaragoz <gkaragoz96@gmail.com>
2020-05-30 21:54:03 +0300

    Create Scaler script and implement it to Power ups.

--- Tower-Style-Game/Assets/Scripts/Utils/Rotator.cs
+++ Tower-Style-Game/Assets/Scripts/Utils/Rotator.cs
@@ -6,22 +6,13 @@ namespace GK {
 
 		[SerializeField]
 		private float _rotationSpeedInSeconds = 0.3f;
-		[SerializeField]
-		private bool _setRandomDelay = true;
 
-		private void Update() {
-			if (Input.GetKeyDown(KeyCode.Space)) {
-				DoRotate();
-			}
-			if (Input.GetKeyDown(KeyCode.A)) {
-				StopRotate();
-			}
+		private void Awake() {
+			DoRotate();
 		}
 
 		public void DoRotate() {
-			if (_setRandomDelay) {
-				LeanTween.rotateAroundLocal(this.gameObject, Vector3.up, 360, _rotationSpeedInSeconds).setLoopClamp();
-			}
+			LeanTween.rotateAroundLocal(this.gameObject, Vector3.up, 360, _rotationSpeedInSeconds).setLoopClamp();
 		}
 
 		public void StopRotate() {
---
+++ Tower-Style-Game/Assets/Scripts/Utils/Scaler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace GK {
+
+	public class Scaler : MonoBehaviour {
+		
+		[SerializeField]
+		private float _scaleAmount = 1.1f;
+		[SerializeField]
+		private float _scaleSpeed = 0.1f;
+
+		private void Awake() {
+			LeanTween.scale(this.gameObject, Vector3.one * _scaleAmount, _scaleSpeed).setEase(LeanTweenType.linear).setLoopPingPong().setIgnoreTimeScale(true);
+		}
+
+	}
+
+}
