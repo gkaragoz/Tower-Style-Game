685d4c2dc239e21800aac112af62fa818eb6d231
YesilyurtGurkan <gurkan@ganover.com>
2020-05-30 09:19:07 +0300

    Initialize UIManager

--- Tower-Style-Game/Assets/Scripts/UIManagement/UIManager.cs
+++ Tower-Style-Game/Assets/Scripts/UIManagement/UIManager.cs
@@ -1,11 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 namespace GY{
 
 public class UIManager : MonoBehaviour
  {
-   
- }
+
+        public GameObject grpGold;
+        public GameObject pnlMainMenu;
+        public GameObject pnlGameplay;
+        public GameObject pnlPause;
+        public GameObject pnlFail;
+        public GameObject pnlWin;
+        public GameObject pnlSure;
+
+        private void Start() {
+                
+        }
+
+
+
+
+    }
 }
