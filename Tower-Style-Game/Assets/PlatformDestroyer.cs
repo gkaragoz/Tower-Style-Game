7d940c7b98c741d47b3fb6ce2a126a6a6fab010a
gkaragoz <gkaragoz96@gmail.com>
2020-05-30 14:09:38 +0300

    Create Shaker and PlatformDestroyer.cs scripts

---
+++ Tower-Style-Game/Assets/PlatformDestroyer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace GK {
+
+	public class PlatformDestroyer : MonoBehaviour {
+
+		[SerializeField]
+		private float _destroyTime = 3f;
+		[SerializeField]
+		private float _scaleYShrinkAmount = 0.75f;
+		[SerializeField]
+		private float _completeShrinkSpeed = 0.3f;
+		[SerializeField]
+		private float _scaleYShrinkSpeed = 0.5f;
+
+		private Shaker[] _shakers = null;
+
+		private void Awake() {
+			_shakers = GetComponentsInChildren<Shaker>();
+		}
+
+		private IEnumerator IStartShake() {
+			foreach (Shaker shaker in _shakers) {
+				shaker.DoShake();
+			}
+
+			yield return new WaitForSeconds(_destroyTime);
+
+			DoShrink();
+		}
+
+		private void DoShrink() {
+			var seq = LeanTween.sequence();
+
+			seq.append(LeanTween.scaleY(this.gameObject, _scaleYShrinkAmount, _scaleYShrinkSpeed).setEaseOutQuad());
+
+			foreach (Shaker shaker in _shakers) {
+				shaker.StopShake();
+			}
+
+			seq.append(LeanTween.scaleX(this.gameObject, 1.1f, _scaleYShrinkSpeed * 0.2f).setEaseOutQuad());
+
+			seq.append(LeanTween.scale(this.gameObject, Vector3.zero, _completeShrinkSpeed).setEaseOutQuad());
+		}
+
+		private void Update() {
+			if (Input.GetKeyDown(KeyCode.Space)) {
+				StartShake();
+			}
+		}
+
+		public void StartShake() {
+			StartCoroutine(IStartShake());
+		}
+		
+	}
+
+}
---
+++ Tower-Style-Game/Assets/Shaker.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace GK {
+
+	public class Shaker : MonoBehaviour {
+
+		[SerializeField]
+		private Vector3 _shakeDirection = Vector3.one;
+
+		[SerializeField]
+		private float _jumpIter = 9.5f;
+		[SerializeField]
+		private float _heightMultiplier = 0.3f;
+		[SerializeField]
+		private float _heightDegreeMultiplier = 0.2f;
+		[SerializeField]
+		private float _shakePeriodTime = 0.42f;
+		[SerializeField]
+		private float _dropOffTime = 1.6f;
+
+		public void DoShake() {
+			float height = Mathf.PerlinNoise(_jumpIter, 0f) * 10f;
+			height = height * height * _heightMultiplier;
+			float shakeAmt = height * _heightDegreeMultiplier; // the degrees to shake the camera
+			float shakePeriodTime = _shakePeriodTime; // The period of each shake
+			float dropOffTime = _dropOffTime; // How long it takes the shaking to settle down to nothing
+
+			LTDescr shakeTweenRight = LeanTween.rotateAroundLocal(gameObject, _shakeDirection, shakeAmt, shakePeriodTime)
+			.setEase(LeanTweenType.easeShake) // this is a special ease that is good for shaking
+			.setLoopClamp()
+			.setRepeat(-1);
+
+			//// Slow the camera shake down to zero
+			//LeanTween.value(gameObject, shakeAmt, 0f, dropOffTime).setOnUpdate(
+			//	(float val) => {
+			//		shakeTween.setTo(direction * val);
+			//	}
+			//).setEase(LeanTweenType.easeOutQuad);
+		}
+
+		public void StopShake() {
+			LeanTween.cancel(this.gameObject);
+		}
+
+	}
+
+}
