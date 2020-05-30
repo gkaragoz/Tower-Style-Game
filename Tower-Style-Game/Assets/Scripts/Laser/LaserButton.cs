0e8272acd8da4ca67b0d41fd75e6d97b7b9fa208
gkaragoz <gkaragoz96@gmail.com>
2020-05-30 22:06:08 +0300

    Create LaserInteractTween and implement it to LaserButton.

--- Tower-Style-Game/Assets/Scripts/Laser/LaserButton.cs
+++ Tower-Style-Game/Assets/Scripts/Laser/LaserButton.cs
@@ -1,14 +1,27 @@
 ﻿using UnityEngine;
+using GK;
 
-namespace GY{
+namespace GY {
 
-public class LaserButton : MonoBehaviour
- {
+    public class LaserButton : MonoBehaviour {
+
         [SerializeField]
-        private LaserHolder _laserHolder=null;
+        private LaserHolder _laserHolder = null;
+        [SerializeField]
+        private LaserInteractTween _laserInteractTween = null;
+
+        [Utils.ReadOnly]
+        [SerializeField]
+        private bool _hasInteracted = false;
 
         public void CloseLaser() {
+            if (_hasInteracted) {
+                return;
+            }
+
+            _hasInteracted = true;
+            _laserInteractTween.Interact();
             _laserHolder.CloseLaser();
         }
- }
+    }
 }
---
+++ Tower-Style-Game/Assets/Scripts/Laser/LaserInteractTween.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace GK {
+
+	public class LaserInteractTween : MonoBehaviour {
+
+		[SerializeField]
+		private GameObject _target = null;
+		[SerializeField]
+		private float _rotateTo = 120;
+		[SerializeField]
+		private float _rotationSpeed = 1f;
+
+		public void Interact() {
+			if (LeanTween.isTweening(_target)) {
+				return;
+			}
+
+			LeanTween.rotateAroundLocal(_target, Vector3.forward, _rotateTo, _rotationSpeed);
+		}
+		
+	}
+
+}
