9cd1ee29877eb1664d5e5fd0bbd53cd08d0e1b19
gkaragoz <gkaragoz96@gmail.com>
2020-06-02 13:32:30 +0300

    Bake scene via cubemaps. Close Camera Controller scripts.

---
+++ Tower-Style-Game/Assets/Scripts/Utils/FPS.cs
@@ -0,0 +1,50 @@
+﻿using TMPro;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class FPS : MonoBehaviour {
+    public float updateInterval = 0.5F;
+    public TextMeshProUGUI txtFps;
+
+    public bool limitFrameRate = false;
+    [Range(30, 60)]
+    public int maxApplicationFrameRate = 30;
+
+    private float _accum = 0;    // FPS accumulated over the interval
+    private int _frames = 0;     // Frames drawn over the interval
+    private float _timeleft;     // Left time for current interval
+
+    private void Start() {
+        _timeleft = updateInterval;
+
+        if (limitFrameRate) {
+            Application.targetFrameRate = maxApplicationFrameRate;
+        }
+    }
+
+    private void Update() {
+        _timeleft -= Time.deltaTime;
+        _accum += Time.timeScale / Time.deltaTime;
+        ++_frames;
+        // Interval ended - update GUI text and start new interval
+        if (_timeleft <= 0.0) {
+            // display two fractional digits (f2 format)
+            float fps = _accum / _frames;
+            string format = System.String.Format("{0:F2} FPS", fps);
+            txtFps.text = format;
+
+            if (fps < 30) {
+                txtFps.color = Color.yellow;
+            } else if (fps < 10) {
+                txtFps.color = Color.red;
+            } else {
+                txtFps.color = Color.green;
+            }
+
+            _timeleft = updateInterval;
+            _accum = 0.0F;
+            _frames = 0;
+        }
+    }
+
+}
