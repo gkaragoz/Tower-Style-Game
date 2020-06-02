ff6e581d642ace46a57e3fdc9d47e6ee084991a7
YesilyurtGurkan <gurkan@ganover.com>
2020-06-02 19:05:20 +0300

    Fix death event and camera falling with player

--- Tower-Style-Game/Assets/Scripts/Camera/CameraParentFollower.cs
+++ Tower-Style-Game/Assets/Scripts/Camera/CameraParentFollower.cs
@@ -8,10 +8,18 @@ public class CameraParentFollower : MonoBehaviour
  {
         public Transform player;
         Vector3 _myPos;
+        private CameraDeathPosition _cameraDeathPosition;
+
+        private void Start() {
+            _cameraDeathPosition = GetComponentInChildren<CameraDeathPosition>();
+        }
         private void Update() {
-            _myPos = transform.position;
-            _myPos.y = player.transform.position.y;
-            transform.position=_myPos;
+            if (player.position.y>_cameraDeathPosition.LavaYPosition()) {
+                _myPos = transform.position;
+                _myPos.y = player.transform.position.y;
+                transform.position = _myPos;
+            }
+            
         }
     }
 }
