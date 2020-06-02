87b55ed6fcc321e8a1fd7c1e4b95ee0e4e9761a1
YesilyurtGurkan <gurkan@ganover.com>
2020-06-02 13:03:14 +0300

    Import Polygon Dungeon asset

---
+++ Tower-Style-Game/Assets/Scripts/Lights/GoldLight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GY {
+
+    public class GoldLight : MonoBehaviour {
+        public Light light;
+        public float minVal = 3;
+        public float maxVal = 8;
+        public float speed;
+        public LeanTweenType type;
+        private void Awake() {
+
+            LeanTween.value(this.gameObject, minVal, maxVal, speed).setLoopPingPong().setEase(type).setOnUpdate((float value) => {
+                light.intensity = value;
+            });
+        }
+    }
+}
