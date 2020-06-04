b28f4277751020ac56bc15c23edeec430b82a8fc
gkaragoz <gkaragoz96@gmail.com>
2020-06-04 16:03:43 +0300

    Implement collect power up UI animations.

---
+++ Tower-Style-Game/Assets/Scripts/UIManagement/PowerUpTween.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GK {
+
+	public class PowerUpTween : MonoBehaviour {
+
+		[SerializeField]
+		private Image _image = null;
+
+		private void Update() {
+			if (Input.GetKeyDown(KeyCode.Space)) {
+				GetPowerUp();
+			}
+		}
+
+		public void GetPowerUp() {
+			Vector3 screenPos = Vector3.zero;
+
+			_image.rectTransform.anchoredPosition = screenPos;
+			_image.gameObject.SetActive(true);
+		}
+
+	}
+
+}
