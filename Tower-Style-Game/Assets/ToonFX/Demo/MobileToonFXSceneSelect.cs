ed8b8ca2b7e15cf65db213dc966164f054f2892a
YesilyurtGurkan <gurkan@ganover.com>
2020-06-04 12:35:18 +0300

    Ad Dust Fx to player

---
+++ Tower-Style-Game/Assets/ToonFX/Demo/MobileToonFXSceneSelect.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MobileToonFXSceneSelect : MonoBehaviour
+{
+    public void LoadSceneDemo1()
+    {
+        SceneManager.LoadScene("MobileToonFXScene1");
+    }
+    public void LoadSceneDemo2()
+    {
+        SceneManager.LoadScene("MobileToonFXScene2");
+    }
+    public void LoadSceneDemo3()
+    {
+        SceneManager.LoadScene("MobileToonFXScene3");
+    }
+    public void LoadSceneDemo4()
+    {
+        SceneManager.LoadScene("MobileToonFXScene4");
+    }
+    public void LoadSceneDemo5()
+    {
+        SceneManager.LoadScene("MobileToonFXScene5");
+    }
+    public void LoadSceneDemo6()
+    {
+        SceneManager.LoadScene("MobileToonFXScene6");
+    }
+    public void LoadSceneDemo7()
+    {
+        SceneManager.LoadScene("MobileToonFXScene7");
+    }
+    public void LoadSceneDemo8()
+    {
+        SceneManager.LoadScene("MobileToonFXScene8");
+    }
+    public void LoadSceneDemo9()
+    {
+        SceneManager.LoadScene("MobileToonFXScene9");
+    }
+    public void LoadSceneDemo10()
+    {
+        SceneManager.LoadScene("MobileToonFXScene10");
+    }
+	public void LoadSceneDemo11()
+    {
+        SceneManager.LoadScene("MobileToonFXScene11");
+    }
+	public void LoadSceneDemo12()
+    {
+        SceneManager.LoadScene("MobileToonFXScene12");
+    }
+	public void LoadSceneDemo13()
+    {
+        SceneManager.LoadScene("MobileToonFXScene13");
+    }
+	public void LoadSceneDemo14()
+    {
+        SceneManager.LoadScene("MobileToonFXScene14");
+    }
+}
