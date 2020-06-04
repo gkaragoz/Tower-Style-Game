14a027d7b4d91ab7108db970f921533e63920eca
YesilyurtGurkan <gurkan@ganover.com>
2020-06-04 19:04:28 +0300

    KOMPLE BAKIM YAPILDI. ELDEN GEÇİRİLDİ. ON KAPUT BOYALI

---
+++ Tower-Style-Game/Assets/Scripts/Platform/PlatformCollision.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+[ExecuteAlways]
+public class PlatformCollision : MonoBehaviour {
+    public enum PlatformType {
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six
+    }
+
+    [SerializeField]
+    private PlatformType platformType = PlatformType.Six;
+
+    [System.Serializable]
+    public class CollisionData {
+        public BoxCollider2D collider;
+        public GameObject platform;
+    }
+
+    public CollisionData[] collisions;
+
+    private void Awake() {
+        UpdatePlatforms();
+    }
+    
+    private void UpdatePlatforms() {
+        foreach (var item in collisions) {
+            item.platform.gameObject.SetActive(false);
+            item.collider.enabled = false;
+        }
+
+        collisions[(int)platformType].collider.enabled = true;
+        collisions[(int)platformType].platform.SetActive(true);
+    }
+
+    private void OnValidate() {
+        UpdatePlatforms();
+    }
+
+}
