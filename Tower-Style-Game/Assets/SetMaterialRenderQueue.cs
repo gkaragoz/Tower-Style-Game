5b6d3d5a4313d907c91cf3961782c8b061d0c963
gkaragoz <gkaragoz96@gmail.com>
2020-06-04 13:16:22 +0300

    Create SetMaterialRenderQueue prefab. Need to implement under the GameMechanics prefab.

---
+++ Tower-Style-Game/Assets/SetMaterialRenderQueue.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace GK {
+
+	public class SetMaterialRenderQueue : MonoBehaviour {
+
+		[System.Serializable]
+		public class RQMaterial {
+			public Material material;
+			[Range(-1, 5000)]
+			public int renderOrder = 2000;
+		}
+
+		[SerializeField]
+		private RQMaterial[] _rqMaterials = null;
+
+		private void Start() {
+			foreach (RQMaterial rqMaterial in _rqMaterials) {
+				rqMaterial.material.renderQueue = rqMaterial.renderOrder;
+			}
+		}
+
+	}
+
+}
