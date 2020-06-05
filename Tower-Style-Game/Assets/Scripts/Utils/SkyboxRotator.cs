3a31806ccc8a9659c98b5d2145b6fdc31d8ee537
gkaragoz <gkaragoz96@gmail.com>
2020-06-05 18:20:38 +0300

    Create and implement SkyboxRotator.cs

---
+++ Tower-Style-Game/Assets/Scripts/Utils/SkyboxRotator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace GK {
+
+	public class SkyboxRotator : MonoBehaviour {
+
+		[SerializeField]
+		private float _rotationSpeed = 13f;
+
+		private void Update() {
+			RenderSettings.skybox.SetFloat("_Rotation", Time.time * _rotationSpeed);
+		}
+
+	}
+
+}
