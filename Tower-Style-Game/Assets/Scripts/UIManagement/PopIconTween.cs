33aec22cf7afe7ea0e3e70b96033bf6753b4f742
gkaragoz <gkaragoz96@gmail.com>
2020-06-05 18:13:08 +0300

    Implement UI for watch ads to get double rewards and skip level.

---
+++ Tower-Style-Game/Assets/Scripts/UIManagement/PopIconTween.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace GK {
+
+    public class PopIconTween : MonoBehaviour {
+
+        [SerializeField]
+        private RectTransform _targetTween = null;
+        [SerializeField]
+        private float _delaySpeed = 0.5f;
+        [SerializeField]
+        private float _scaleAmount = 1.1f;
+        [SerializeField]
+        private float _scaleSpeed = 0.1f;
+
+        private void Awake() {
+            LeanTween.delayedCall(_delaySpeed, () => {
+                // Pop size of button
+                LeanTween.scale(_targetTween, Vector3.one * _scaleAmount, _scaleSpeed).setLoopPingPong().setEaseInOutBack().setRepeat(2);
+            }).setRepeat(-1);
+        }
+
+    }
+
+}
