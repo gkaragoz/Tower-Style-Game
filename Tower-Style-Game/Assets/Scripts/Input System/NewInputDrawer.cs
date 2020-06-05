41a1c3fd79a75aca82133a4f3726dd11ba398edd
gkaragoz <gkaragoz96@gmail.com>
2020-06-05 15:29:24 +0300

    Remove unnecessary debug log.

--- Tower-Style-Game/Assets/Scripts/Input System/NewInputDrawer.cs
+++ Tower-Style-Game/Assets/Scripts/Input System/NewInputDrawer.cs
@@ -7,13 +7,13 @@ namespace GK {
         [SerializeField]
         private Transform _targetTransform = null;
         [SerializeField]
-        private Vector2 _drawOffset = Vector2.zero;  
+        private Vector2 _drawOffset = Vector2.zero;
         [SerializeField]
         private float _projectileLengthMultiplier = 1;
         [SerializeField]
         LineRenderer _lr;
         [SerializeField]
-        private int _indicatorPointCount=2;
+        private int _indicatorPointCount = 2;
         [SerializeField]
         private Gradient _indicatorLowPowerPalette;
         [SerializeField]
@@ -26,12 +26,12 @@ namespace GK {
             InputManager.instance.OnInputBegin += OnInputBegin;
             InputManager.instance.OnInputDragging += OnInputDragging;
             InputManager.instance.OnInputEnd += OnInputEnd;
-            _lr.positionCount = _indicatorPointCount;         
+            _lr.positionCount = _indicatorPointCount;
         }
 
         private void OnInputBegin(Vector2 startPos) {
             _lr.enabled = true;
-            
+
         }
 
         private void OnInputDragging(Vector2 currentPos, Vector2 dragPos) {
@@ -48,16 +48,14 @@ namespace GK {
             for (int ii = 0; ii < _indicatorPointCount; ii++) {
                 Vector3 lastPos = (_targetTransform.position + new Vector3(_drawOffset.x, _drawOffset.y, 0)) + distanceProjectile * ii * _projectileLengthMultiplier;
                 _lr.SetPosition(ii, lastPos);
-                Debug.Log(distanceProjectile.magnitude);
-                if (distanceProjectile.magnitude<0.2f) {
-                    _lr.colorGradient = _indicatorLowPowerPalette;
-                } else if (distanceProjectile.magnitude<.4f) {
-                    _lr.colorGradient = _indicatorMediumPowerPalette;
-                }else if (distanceProjectile.magnitude<.5f) {
-                    _lr.colorGradient = _indicatorHighPowePalette;
 
+                if (distanceProjectile.magnitude < 0.2f) {
+                    _lr.colorGradient = _indicatorLowPowerPalette;
+                } else if (distanceProjectile.magnitude < .4f) {
+                    _lr.colorGradient = _indicatorMediumPowerPalette;
+                } else if (distanceProjectile.magnitude < .5f) {
+                    _lr.colorGradient = _indicatorHighPowePalette;
                 }
-                
             }
         }
     }
